01047bd8d03655b3a5b7b18c5be8a59a76603210
gabrielminamoto@gmail.com <gabrielminamoto@gmail.com>
2026-02-24 09:40:42 -0300

    Exercicio01 finalizado

---
+++ src/exercicio01/Main.java
@@ -0,0 +1,35 @@
+package exercicio01;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        Random r = new Random();
+        contaBanco c = new contaBanco();
+        double valor;
+
+        System.out.print("Nome do correntista ---> ");
+        c.nome=e.next();
+
+        c.numero=r.nextInt(1000,9000);
+
+        System.out.println(("Seu saldo --> ") + c.saldo);
+
+        //pedindo valor de deposito
+        System.out.println(("Digite o valor de deposito --> "));
+        valor = e.nextDouble();
+        c.deposito(valor);
+
+        System.out.println(("Seu saldo atual --->") + c.saldo);
+
+        System.out.println(("Digite o valor de saque --> "));
+        valor = e.nextDouble();
+        c.sacar(valor);
+
+        System.out.println(("Seu saldo atual ---> ") + c.saldo);
+
+
+    }
+}
---
+++ src/exercicio01/contaBanco.java
@@ -0,0 +1,20 @@
+package exercicio01;
+
+public class contaBanco {
+    //atributos
+    String nome;
+    int numero;
+    double saldo;
+
+    //metodo para depositar valor
+    //evitar fazer scanner dentro de metodos, focar em montar o mais generico possivel
+    public void deposito(double valor){
+
+        saldo += valor;
+    }
+
+    //metodo para sacar valor
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+}
