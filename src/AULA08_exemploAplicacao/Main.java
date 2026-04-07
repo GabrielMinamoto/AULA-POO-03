97345e021abebae28593ff6f56553fc631b2f3bf
Laboratório FIAP <gabrielminamoto@gmail.com>
2026-04-07 11:36:50 -0300

    Aplicando set

--- src/AULA08_exemploAplicacao/Main.java
+++ src/AULA08_exemploAplicacao/Main.java
@@ -3,6 +3,8 @@ package AULA08_exemploAplicacao;
 public class Main {
     public static void main(String[] args) {
         Paciente p = new Paciente("Kenny", 1.50);
+        p.setNome("Arthur");
+        p.setAltura(1.90);
         System.out.println(p.getNome());
     }
 }
