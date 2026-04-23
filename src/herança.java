971503ce9f79d2ae6826c795329b7e7cb0169971
Laboratório FIAP <gabrielminamoto@gmail.com>
2026-04-23 08:43:40 -0300

    Herança

---
+++ src/herança.java
@@ -0,0 +1,11 @@
+public class herança {
+
+//    para descobrir se é heranca, faça a pergunta "é um?" -> se sim herança, senão não tem
+//    se não tem tipo, pode criar um tipo para ter herança -> superclasse -> Object
+//    public class (SubClasse) extends (SuperClasse) -> como realizar herança
+//    (SubClasse) é do tipo (SuperClasse)
+//    composicao -> se a resposta for sim para "tem um?", quer dizer que é composicao
+//    para chamar o construtor da SuperClase -> super()
+//    preciso chamar o construtor da SuperClasse para pegar os atributos da superclasse
+
+}
